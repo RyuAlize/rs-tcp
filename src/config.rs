9c4e0fd725e9c97c545f8f5c0aea00519483a0a0
RyuAlize <3143157934@qq.com>
2022-05-26 22:45:18 +0800

    Add Config

---
+++ src/config.rs
@@ -0,0 +1,5 @@
+pub const TOPOLOGY_NAME_SIZE: usize = 16;
+pub const NODE_NAME_SIZE: usize = 16;
+pub const IF_NAME_SIZE: usize = 16;
+pub const MAX_INTF_PER_NODE: usize = 10;
+pub const MAX_PACKET_BUFFER_SIZE: usize = 1024;
